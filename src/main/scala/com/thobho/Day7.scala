8fb2154a6f8213ac5a420e4b829cd239222045b6
tomasz.bar <tomasz.bar@ocado.com>
2021-12-07 11:02:19 +0100

    day-7

--- src/main/scala/com/thobho/Day7.scala
+++ src/main/scala/com/thobho/Day7.scala
@@ -1,5 +1,24 @@
 package com.thobho
 
+import com.thobho.Utils.readInputLines
+
 object Day7 {
+
+  private val inputPath = "resources/day-7-input.txt"
+
+  def solve(input:List[Int], fuelFun: (Int, Int) => Int):Int =
+    (input.min to input.max).map(target => input.map(number => fuelFun(target, number)).sum).min
+
+  val constant: (Int, Int) => Int = (target, position) => Math.abs(target - position)
+  val linear: (Int, Int) => Int = (target, position) => ((1 + Math.abs(target - position)) * Math.abs(target - position)) / 2
+
+
+  def main(args: Array[String]): Unit = {
+    val input = readInputLines(inputPath).head.split(",").toList.map(_.toInt)
+    val solutionOne = solve(input, constant)
+    val solutionTwo = solve(input, linear)
+    println(solutionOne)
+    println(solutionTwo)
+  }
 
 }
