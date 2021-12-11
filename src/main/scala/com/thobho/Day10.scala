2e2b14181db9eb770953641dfa142cdacc752b7e
tomasz.bar <tomasz.bar@ocado.com>
2021-12-11 01:56:05 +0100

    day-10

--- src/main/scala/com/thobho/Day10.scala
+++ src/main/scala/com/thobho/Day10.scala
@@ -1,6 +1,6 @@
 package com.thobho
 
-import com.thobho.Day10.Token.{closeToOpenPair, openToClosePair, openingTokes, points}
+import com.thobho.Day10.Token.{closeToOpenPair, correctTokenPoints, openToClosePair, wrongTokenPoints}
 
 import scala.annotation.tailrec
 
@@ -8,67 +8,66 @@ object Day10 {
 
   private val inputPath = "resources/day-10-input.txt"
 
-
   object Token {
-    val openingTokes = List('{','(','[','<')
-    val closeToOpenPair = Map(
-      '}' -> '{',
-      ']' -> '[',
-      ')' -> '(',
-      '>' -> '<'
-    )
-    val openToClosePair: Map[Char, Char] = closeToOpenPair
-      .map {case (k,v) => (v,k)}
-
-    val points = Map(
-    ')'-> 3,
-    ']'-> 57,
-  '}'-> 1197,
-  '>'-> 25137
-    )
+    val closeToOpenPair = Map('}' -> '{', ']' -> '[', ')' -> '(', '>' -> '<')
+    val openToClosePair: Map[Char, Char] = closeToOpenPair.map { case (k, v) => (v, k) }
+    val wrongTokenPoints = Map(')' -> BigInt(3), ']' -> BigInt(57), '}' -> BigInt(1197), '>' -> BigInt(25137))
+    val correctTokenPoints = Map(')' -> BigInt(1), ']' -> BigInt(2), '}' -> BigInt(3), '>' -> BigInt(4))
   }
 
   case class Token(value: Char) {
-    def isOpening:Boolean = openingTokes.contains(value)
-    def isClosing:Boolean = !isOpening
+    def isOpening: Boolean = openToClosePair.keys.toList.contains(value)
+
+    def isClosing: Boolean = !isOpening
+
     def matchesStart(startTag: Token): Boolean = closeToOpenPair(this.value) == startTag.value
   }
 
 
   @tailrec
-  def parseBrackets(input: List[Token], stack: List[Token] = Nil, current:Option[Char] = None): (List[Token], List[Token], Option[Char]) =
+  def parseBrackets(input: List[Token], stack: List[Token] = Nil, current: Option[Char] = None): (List[Token], List[Token], Option[Char]) =
     input match {
-      case Nil => (Nil,Nil, current)
-      case head::tail =>
-        if(head.isOpening) parseBrackets(tail, head :: stack, Some(head.value))
-        else if(head.isClosing && head.matchesStart(stack.head)) {
+      case Nil => {
+        (input, stack, current)
+      }
+      case head :: tail =>
+        if (head.isOpening) parseBrackets(tail, head :: stack, Some(head.value))
+        else if (head.isClosing && head.matchesStart(stack.head)) {
           parseBrackets(tail, stack.tail, Some(head.value))
         } else (tail, stack, Some(head.value))
     }
 
-  def solvePartOne(input:List[List[Token]]): Int = {
+  def solvePartOne(input: List[List[Token]]): BigInt = {
     input
       .map(parseBrackets(_))
-      .filter {case (_, stack, _) => stack.nonEmpty}
-      .map {case (_, _, current) => current}
-      .map(wrongToken => points(wrongToken.get))
+      .filter { case (rest, _, _) => rest.nonEmpty }
+      .map { case (_, _, current) => current }
+      .map(wrongToken => wrongTokenPoints(wrongToken.get))
       .sum
   }
 
-  def solvePartTwo(input:List[List[Token]]): Unit = {
+  def solvePartTwo(input: List[List[Token]]): BigInt = {
     val result = input
       .map(parseBrackets(_))
-      .filter {case (_, stack, _) => stack.isEmpty}
+      .filter { case (rest, _, _) => rest.isEmpty }
+      .map { case (_, stack, _) => stack.map(token => openToClosePair(token.value)) }
+      .map { tokens => tokens.map(correctTokenPoints(_)).reverse }
+      .map { points => points.foldRight(BigInt(0))((n, acc) => acc * 5 + n) }
 
-    println(result)
+
+
+    oddLengthListMedian(result)
+  }
+
+  def oddLengthListMedian(list: List[BigInt]): BigInt = {
+    list.sortWith(_ < _).drop(list.length/2).head
   }
 
   def main(args: Array[String]): Unit = {
     val tokens = Utils.readInputLines(inputPath)
       .map(inputLine => inputLine.map(Token(_)).toList)
 
-
-    solvePartTwo(tokens)
-//    print(solvePartOne(tokens))
+    println(solvePartOne(tokens))
+    println(solvePartTwo(tokens))
   }
 }
